3acbeb1741dd94c63c2cbdf65033ebe17c058cd3
sca-shun-sato0013 <sdt22cge0013@edu.sca.ac.jp>
2022-10-17 14:33:03 +0900

    stage選択画面のスクロール修正と看守イラストの差し替え

---
+++ Innocence/Assets/01_Scripts/StageSelectScreen/Scroll.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] GameObject obj; //移動させる対象
+
+    Vector3 targetPos; //移動先
+    float currentY = 0f;　//移動先(y)
+
+    [SerializeField] float speed = 1.0f; //移動速度
+    private void Start()
+    {
+        //初期化
+        targetPos = obj.transform.position; //初期位置
+        currentY = targetPos.y;
+    }
+    void Update()
+    {
+       
+        Debug.Log(targetPos != obj.transform.position);
+        Debug.Log(currentY <= 18f);
+        Debug.Log(currentY >= 250f);
+
+       
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //上限
+            if (currentY <= 18f)
+            {
+                return;
+            }
+            //移動先設定
+            currentY -= 18f; //Ý軸のみ変更
+            targetPos.y = currentY;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //上限
+            if (currentY >= 210f)
+            {
+                return;
+            }
+            //移動先設定
+            currentY += 18f;
+            targetPos.y = currentY;
+        }
+
+        //移動先!＝現在の位置の場合の時移動
+        if (targetPos != obj.transform.position)
+        {
+            //移動
+            //移動対象 = (移動元,移動先,速度)
+            obj.transform.position = Vector3.MoveTowards(obj.transform.position, targetPos, speed * Time.deltaTime);
+            return;
+        }
+
+    }
+}
